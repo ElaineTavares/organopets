9828d9aee75a76967ec85cd4cb6a5f7217f10ab4
Elaine Tavares <elainetavares.dev@gmail.com>
2024-06-04 17:45:28 -0300

    Cards dos pets

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './Pet.css'
+import { IoIosCloseCircle } from "react-icons/io";
 
 export default function Pet(props) {
   return (
     <div className='pet'>
+        <IoIosCloseCircle 
+            size={25} 
+            className='deletar' 
+            onClick={props.aoDeletar}
+        />
         <div className='cabecalho' style={{backgroundColor: props.corDeFundo}}>
             <img src={props.imagem} alt={props.nome} />
         </div>
--- src/Time.jsx
+++ src/Time.jsx
@@ -3,11 +3,22 @@ import './Time.css'
 import Pet from './Pet'
 
 export default function Time(props) {
+
   return (
     (props.pets.length > 0) && <section className='time' style={{backgroundColor: props.corSecundaria}}>
+                                  <div className='input_cor'>
+                                    <p>Mude de cor</p>
+                                    <input  value={props.corPrimaria} onChange={evento=> props.mudarCor(evento.target.value, props.nome)}  type="color" />
+                                  </div>  
+                                  <div className='input_cor_secundaria'>
+                                    <p>Mude de cor</p>
+                                    <input  value={props.corSecundaria} onChange={evento=> props.mudarCorSecundaria(evento.target.value, props.nome)}  type="color" />
+                                  </div>   
                                   <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
                                   <div className="pets">
-                                    {props.pets.map(pet => <Pet corDeFundo={props.corPrimaria}  key={pet.nome} nome={pet.nome} raca={pet.raca} imagem={pet.imagem}/>)}
+                                    {props.pets.map(pet => {
+                                      return <Pet corDeFundo={props.corPrimaria}  key={pet.nome} nome={pet.nome} raca={pet.raca} imagem={pet.imagem} aoDeletar={props.aoDeletar}/>
+                                    })}
                                   </div>  
                                </section>
     
